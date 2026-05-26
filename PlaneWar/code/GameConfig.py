54cf38fe2805c31358d1f6cc3a4343f271efb37e
Xuyutong121 <2937856749@qq.com>
2026-05-26 15:10:52 +0800

    Add files via upload

---
+++ PlaneWar/code/GameConfig.py
@@ -0,0 +1,16 @@
+# 游戏的配置模块（设置窗口尺寸、图片、音频、字体的路径）
+
+# 配置窗口尺寸
+WIDTH = 480
+HEIGHT = 852
+SCREEN_SIZE = (WIDTH, HEIGHT)
+
+# 背景图片路径（相对路径）
+BG_IMG = "../images/background.png"
+
+# LOGO图片路径
+ICON_IMG = "../images/icon.png"
+
+# 定义飞机图片路径
+PLANE1_IMG = "../images/hero1.png"
+PLANE2_IMG = "../images/hero2.png"
---
+++ PlaneWar/code/main.py
@@ -0,0 +1,7 @@
+# 游戏启动模块
+
+from GameView import GameView
+
+if __name__ == '__main__':
+    view = GameView()
+    view.main()
